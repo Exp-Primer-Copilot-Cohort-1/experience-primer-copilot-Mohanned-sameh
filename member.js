e3ef79c582e3890c3be86ef4317ec8b7c3281064
Mohanned-sameh <110717277+Mohanned-sameh@users.noreply.github.com>
2024-01-16 02:34:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'partials/member.html',
+    controller: 'MemberController',
+    controllerAs: 'memberCtrl',
+    scope: {
+      member: '='
+    }
+  };
+}
